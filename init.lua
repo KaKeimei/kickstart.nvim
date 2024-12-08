3ad238439a543f98913cd3786c56c34de8869c77
KaKeimei <hqm237594742@gmail.com>
2024-12-08 14:16:40 +0900

    change formatter for typescript

--- init.lua
+++ init.lua
@@ -713,6 +713,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        typscript = { 'prettierd', 'prettier' },
+        typescriptreact = { 'prettierd', 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
