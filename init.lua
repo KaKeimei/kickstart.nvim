1fb1ccfdb43c38f88e2c4f88c5f3dd67da259082
KaKeimei <hqm237594742@gmail.com>
2026-04-01 12:14:27 +0900

    add python treesitter parser for syntax highlighting

--- init.lua
+++ init.lua
@@ -896,7 +896,7 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'go', 'html', 'java', 'json', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'toml', 'vim', 'vimdoc', 'yaml' }
+      local parsers = { 'bash', 'c', 'diff', 'go', 'html', 'java', 'json', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'query', 'toml', 'vim', 'vimdoc', 'yaml' }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
